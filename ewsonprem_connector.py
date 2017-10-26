f57f99507dd41ad8aff3b84527b6ad5aedad5be2
michael <michael@phantom.us>
2017-10-26 14:09:42 -0400

    PAPP-3399 Updated recursive to be optional

--- ewsonprem_connector.py
+++ ewsonprem_connector.py
@@ -1332,7 +1332,7 @@ class EWSOnPremConnector(BaseConnector):
         action_result.update_summary({'total_entries': len(mailboxes)})
 
         for mailbox in mailboxes:
-            if param['recursive'] and "DL" in mailbox['t:MailboxType']:
+            if param.get('recursive', False) and "DL" in mailbox['t:MailboxType']:
                 param[EWSONPREM_JSON_GROUP] = mailbox['t:EmailAddress']
                 self._expand_dl(param)
             self._cleanse_key_names(mailbox)
