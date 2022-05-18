b784274065ad961a592cb6ec193a1c8f004f5048
dhwanis-crest <dhwanis@splunk.com>
2022-05-18 16:50:04 +0530

    Removing unused code for different auth mechanisms

--- ewsonprem_consts.py
+++ ewsonprem_consts.py
@@ -32,8 +32,6 @@ EWSONPREM_SEARCH_FINISHED_STATUS = "Finished Searching {0:.0%}"
 
 EWS_JSON_POLL_USER = "poll_user"
 EWS_JSON_USE_IMPERSONATE = "use_impersonation"
-EWS_JSON_AUTH_TYPE = "auth_type"
-EWS_JSON_CLIENT_ID = "client_id"
 EWS_JSON_POLL_FOLDER = "poll_folder"
 EWS_JSON_INGEST_MANNER = "ingest_manner"
 EWS_JSON_INGEST_TIME = "ingest_time"
@@ -41,17 +39,12 @@ EWS_JSON_FIRST_RUN_MAX_EMAILS = "first_run_max_emails"
 EWS_JSON_POLL_MAX_CONTAINERS = "max_containers"
 EWS_JSON_DONT_IMPERSONATE = "dont_impersonate"
 EWS_JSON_IMPERSONATE_EMAIL = "impersonate_email"
-EWS_JSON_AUTH_URL = "authority_url"
-EWS_JSON_FED_PING_URL = "fed_ping_url"
-EWS_JSON_FED_VERIFY_CERT = "fed_verify_server_cert"
 EWS_JSON_IS_PUBLIC_FOLDER = "is_public_folder"
 
 EWSONPREM_ERR_CONNECTIVITY_TEST = "Test Connectivity Failed"
 EWSONPREM_SUCC_CONNECTIVITY_TEST = "Test Connectivity Passed"
 EWSONPREM_ERR_SERVER_CONNECTION = "Connection failed"
-EWSONPREM_ERR_FED_PING_URL = "Parameter validation failed for the Federated Auth Ping URL"
 EWSONPREM_ERR_FROM_SERVER = "API failed. Status code: {code}. Message: {message}"
-EWSONPREM_ERR_API_UNSUPPORTED_METHOD = "Unsupported method"
 EWSONPREM_USING_BASE_URL = "Using url: {base_url}"
 EWSONPREM_ERR_VAULT_INFO = "Could not retrieve vault file"
 EWSONPREM_ERR_JSON_PARSE = "Unable to parse reply, raw string reply: '{raw_text}'"
@@ -69,19 +62,11 @@ EWSONPREM_MAIL_TYPES = [
     "t:MeetingCancellation"
 ]
 EWSONPREM_MAX_END_OFFSET_VAL = 2147483646
-EWS_O365_RESOURCE = "https://outlook.office365.com"
-EWS_LOGIN_URL = "https://login.windows.net"
-
 EWS_MODIFY_CONFIG = "Toggling the impersonation configuration on the asset might help, or login user does not have privileges to the mailbox."
 
 EWS_INGEST_LATEST_EMAILS = "latest first"
 EWS_INGEST_OLDEST_EMAILS = "oldest first"
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
-
-AUTH_TYPE_AZURE = "Azure"
-AUTH_TYPE_AZURE_INTERACTIVE = "Azure (interactive)"
-AUTH_TYPE_FEDERATED = "Federated"
-AUTH_TYPE_BASIC = "Basic"
 
 DEFAULT_REQUEST_TIMEOUT = 30  # in seconds
 STATE_FILE_CORRUPT_ERR = (
